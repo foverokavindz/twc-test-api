1b4dd326088facd752cb200c5d5c334138373e71
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2024-03-31 23:24:07 +0530

    Update server port and add Node.js version in package.json

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
 });
 
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
